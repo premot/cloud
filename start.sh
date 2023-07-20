c7100f18de823e97bd8f345ad2729d1a8b579f7a
premot <126085020+premot@users.noreply.github.com>
2023-07-20 10:27:51 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax
 cp cloud/.* . -r
 #https://distfiles.gentoo.org/releases/amd64/autobuilds/20230716T164653Z/stage3-amd64-openrc-20230716T164653Z.tar.xz
 wget https://distfiles.gentoo.org/releases/arm64/autobuilds/20230716T231652Z/stage3-arm64-openrc-20230716T231652Z.tar.xz
-sudo su -c "mkdir /mnt/gentoo && tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner && sh mounts.sh"
+sudo su -c "mkdir /mnt/gentoo && tar xpvf stage3-*.tar.xz /mnt/gentoo--xattrs-include='*.*' --numeric-owner && sh mounts.sh"
 chsh $USER -s /bin/zsh
