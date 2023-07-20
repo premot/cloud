d1f6ea8a0e5b32a178741f9a56e983e461bd25e3
premot <premote@proton.me>
2023-07-20 17:05:51 +0300

    update

--- start.sh
+++ start.sh
@@ -7,5 +7,4 @@ cp cloud/.* . -r
 chsh $USER -s /bin/zsh
 #x86 version: https://distfiles.gentoo.org/releases/amd64/autobuilds/20230716T164653Z/stage3-amd64-openrc-20230716T164653Z.tar.xz
 wget https://distfiles.gentoo.org/releases/arm64/autobuilds/20230716T231652Z/stage3-arm64-openrc-20230716T231652Z.tar.xz
-sudo su -c "mkdir /mnt/gentoo && tar xpvf stage3-*.tar.xz -C /mnt/gentoo --xattrs-include='*.*' --numeric-owner && cp init.sh /mnt/gentoo/ && sh mounts.sh"
-
+sudo su -c "mkdir /mnt/gentoo && tar xpvf stage3-*.tar.xz -C /mnt/gentoo --xattrs-include='*.*' --numeric-owner && cp init.sh /mnt/gentoo/ && cp swap.sh /mnt/gentoo && sh mounts.sh"
