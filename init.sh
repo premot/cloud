21238a4146faff709918f3484f1fb146caf3dbc8
premot <126085020+premot@users.noreply.github.com>
2023-07-20 11:05:42 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
+source /etc/profile                                                                                         
+export PS1="(chroot) ${PS1}"
 emerge-webrsync
 eselect profile set 4
-/etc/portage/make.conf
+#echo "dev-lang/rust" > /etc/portage/package.mask/zz
+emerge rust-bin
 emerge --ask --verbose --update --deep --newuse @world
