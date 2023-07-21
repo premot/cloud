960d3e30d6f1aaf87dbe4e64ebb027089b9ef570
premot <premote@proton.me>
2023-07-21 14:23:26 +0300

    update

--- adb-mounts.sh
+++ adb-mounts.sh
@@ -7,3 +7,11 @@ busybox mount --bind /run /data/data/mnt/run
 busybox mount --make-slave /data/data/mnt/run
 busybox cp /etc/resolv.conf /data/data/mnt/etc/resolv.conf
 busybox chroot /data/data/mnt /bin/bash
+
+#busybox mount -r -w -t ext2 /dev/block/mmcblk0p1 /data/data/mnt
+#busybox mount -r -w -t ext3 /dev/block/mmcblk1p3 /data/data/mnt
+#busybox mount -r -w -t ext4 /dev/block/mmcblk1p4 /data/data/mnt
+#busybox mount -r -w -t btrfs /dev/block/mmcblk1p5 /data/data/mnt
+#ls /data/data/mnt
+#busybox sh /data/data/mnt/adb-mounts.sh
+#losetup /dev/block/loop0 /data/data/mnt/recovery-cwm6049-atlas40.img
